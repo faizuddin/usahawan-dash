5bbb6b8e593f62969e29f3e459ee624a5108e58b
Faizuddin Mohd Noor <mohd.faizuddin@gmail.com>
2023-09-23 13:38:42 +0800

    first commit

---
+++ dash/funcs.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+
+@st.cache_data
+def load_dataset(filepath):
+    df = pd.read_excel(filepath)
+    return df
+
+def process_data(dataframe):
+    # drop NaNs
+    df = dataframe.dropna()
+
+    # drop Bil column
+    df = df.drop(["Bil"], axis=1)
+
+    # Calculate age by IC number
+    ic = df["No K/P"].str.strip('[]')
+    df["Umur"] = ic.astype(str).str[:2]
+
+    # drop invalid IC numbers
+    df = df[pd.to_numeric(df["Umur"], errors='coerce').notnull()]
+
+    age = df["Umur"].astype(int)
+    df["Umur"] = abs(2023-age).astype(str).str[-2:].astype(int)
+
+    return df
+
+def calc_freq(dataframe, col):
+    freq = dataframe[col].value_counts()
+    # convert series to dataframe
+    freq = freq.reset_index()
+    freq.columns = [col, "Freq"]
+
+    return freq
---
+++ dash/main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import funcs
+
+st.header("Usahawan Dashboard")
+
+df = funcs.process_data(funcs.load_dataset("data/Data Usahawan.xlsx"))
+
+st.write(df)
+
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
+   val = len(df.index) 
+   st.metric("Bilangan rekod", value=val)
+
+with col2:
+   rows = funcs.calc_freq(df, "Agensi")
+   st.metric("Agensi terbesar", value=rows.iloc[0]["Agensi"])
+
+with col3:
+   rows = funcs.calc_freq(df, "Agensi")
+   st.metric("Agensi terkecil", value=rows.iloc[-1]["Agensi"])
+
+with col4:
+   val = sum(df["Umur"])/len(df.index)
+   st.metric("Purata umur usahawan", value="%.2f" % round(val,2))
+
+st.subheader("Summary")
+
+
